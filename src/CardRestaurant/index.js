2051748ea6c35850d27c88e5e59dcb46ddd17995
alifmutaram <alifmutaram22@gmail.com>
2024-10-09 22:42:45 +0700

    add (feat) card restaurant component

---
+++ src/CardRestaurant/index.js
@@ -0,0 +1,23 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "../style.css";
+
+const CardRestaurant = ({ img, location, price, desc, name }) => {
+  return (
+    <div>
+      <Card style={{ width: "16rem", border: "0px solid" }}>
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>{desc}.</Card.Text>
+          <div className="card-title-name">
+            <h6>{location}</h6>
+            <h6>{price}</h6>
+          </div>
+          <Button className="button">LEARN MORE</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+export default CardRestaurant;
