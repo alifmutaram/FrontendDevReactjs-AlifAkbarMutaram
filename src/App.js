b5b4f23991870dd8932c376bd2599efc66be09b5
alifmutaram <alifmutaram22@gmail.com>
2024-10-09 23:36:20 +0700

    change structur app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import RestaurantDetail from "./RestaurantDetail";
 import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        {/* <Route exact path="/restaurant/:resId" component={} /> */}
+        <Route path="/restaurant" component={RestaurantDetail} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
